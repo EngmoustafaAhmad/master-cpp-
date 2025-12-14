501c30109cb25ffbcb2c138085459c033fb0f0f2
Moustafa Ahmad <moustafaahmed51015@gmail.com>
2025-12-14 06:10:50 +0200

    Update function_overloading.cpp

--- 2-functions/function_overloading.cpp
+++ 2-functions/function_overloading.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int add(int a, int b) {
+    return a + b;
+}
+
+double add(double a, double b) {
+    return a + b;
+}
+
+int main() {
+    cout << add(3, 4) << endl;
+    cout << add(2.5, 3.5);
+    return 0;
+}
